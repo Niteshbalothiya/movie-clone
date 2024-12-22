cf7eaeddb7b5aa122cd2c46b8eea2b134068da04
Student ApnaCollege <rs5768827@gmail.com>
2024-12-22 21:05:25 +0530

    frist

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
-import { Home } from './pages/Home'
+import {Home} from './pages/Home'
 import Headers from './components/Header'
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
       <Headers/>
       <Routes>
         <Route  index element = {<Home></Home>}></Route>
-        <Route path='movie/:id'element = {<Home></Home>}></Route>
+        <Route path='movie/:id'element = {<h1>Movice list page</h1>}></Route>
         <Route path='movie/:type'element = {<h1>Movice list page</h1>}></Route>
         <Route path='/*'element = {<h1> Movice list page</h1>}></Route>
       </Routes>
